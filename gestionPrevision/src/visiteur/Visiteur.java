2e08665e1dbc0d55da3afde4fb74e5986fccbc51
scawes <thomas.marescaux@hotmail.fr>
2017-12-12 15:34:55 +0100

    [Visiteur] coefficient

--- gestionPrevision/src/visiteur/Visiteur.java
+++ gestionPrevision/src/visiteur/Visiteur.java
@@ -1,9 +1,16 @@
 package visiteur;
 
 import previsionVents.DonneeVent;
-import previsionVents.ZonePrevision;
+import previsionVents.ListePrevision;
+import previsionVents.Prevision;
 
 public interface Visiteur {
 	void agitSur(ElementVisitable element);
+
+	void agitSur(DonneeVent element);
+
+	void agitSur(ListePrevision element);
+
+	void agitSur(Prevision element);
 
 }
--- gestionPrevision/src/visiteur/VisiteurCoefficient.java
+++ gestionPrevision/src/visiteur/VisiteurCoefficient.java
@@ -1,21 +1,57 @@
 package visiteur;
 
+import previsionVents.DonneeVent;
+import previsionVents.ListePrevision;
+import previsionVents.Prevision;
 import previsionVents.ZonePrevision;
 
 public class VisiteurCoefficient implements Visiteur {
 
-	ZonePrevision zone;
+	ZonePrevision zoneModifier;
+	ZonePrevision zonePrevision;
+	double coefficient;
+	
+	public VisiteurCoefficient(ZonePrevision zone,double coefficient) {
+		this.zoneModifier=zone;
+		if(coefficient<-1 || coefficient > 1) {
+			this.coefficient = 0;
+		} else {
+			this.coefficient=coefficient;
+		}
+	}
+	
+	void calculCoefficient(DonneeVent element) {
+		element.setVitesseVent(
+				element.getVitesseVent() * (1 + coefficient));
+	}
+	
+	@Override
+	public void agitSur(ListePrevision element) {
+		zonePrevision = element.getZonePrevision();
+		for(Prevision prevision : element.getListePrevision() ) {
+			prevision.applique(this);
+		}
+		
+	}
+	
+	@Override
+	public void agitSur(Prevision element) {
+		
+		element.getDonneVent(0,0).applique(this);
+	}
+	
+	@Override
+	public void agitSur(DonneeVent element) {
+		calculCoefficient(element);
+	}
 	
 	@Override
 	public void agitSur(ElementVisitable element) {
+		// TODO Auto-generated method stub
 		
 	}
 
-	public VisiteurCoefficient(ZonePrevision zone) {
-		this.zone=zone;
-	}
 	
-	void calculCoefficient() {
-		
-	}
+
+	
 }
