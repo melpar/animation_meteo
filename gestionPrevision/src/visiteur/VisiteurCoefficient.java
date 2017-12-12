dd53cbccb924f37a4eae4fee62814dff1f8b92bf
scawes <thomas.marescaux@hotmail.fr>
2017-12-12 16:30:11 +0100

    [VISITEUR] coefficient

--- gestionPrevision/src/visiteur/VisiteurCoefficient.java
+++ gestionPrevision/src/visiteur/VisiteurCoefficient.java
@@ -13,16 +13,40 @@ public class VisiteurCoefficient implements Visiteur {
 	
 	public VisiteurCoefficient(ZonePrevision zone,double coefficient) {
 		this.zoneModifier=zone;
-		if(coefficient<-1 || coefficient > 1) {
+		
+		// Le coefficient ne peux pas etre superieur a 1 ou inferieur a -1
+		if(Math.abs(coefficient) > 1) {
 			this.coefficient = 0;
 		} else {
 			this.coefficient=coefficient;
 		}
 	}
 	
-	void calculCoefficient(DonneeVent element) {
-		element.setVitesseVent(
-				element.getVitesseVent() * (1 + coefficient));
+	/**
+	 * modification des vent se trouvant dans la zone.
+	 * @param vent
+	 */
+	
+	void calculCoefficient(DonneeVent vent) {
+		vent.setVitesseVent(
+				vent.getVitesseVent() * (1 + coefficient));
+	}
+	
+	/**
+	 * appel des vent se trouvant dans la prevision.
+	 * @param prevision
+	 */
+	
+	void zoneModifier(Prevision prevision) {
+		for(double positionX = zoneModifier.getLatitudeHautGauche();positionX<zoneModifier.getNombreX()*zoneModifier.getPasX();positionX+=zoneModifier.getPasX()) {
+			for(double positionY = zoneModifier.getLongitudeHautGauche();positionX<zoneModifier.getNombreY()*zoneModifier.getPasY();positionX+=zoneModifier.getPasY()) {
+				DonneeVent vent = prevision.getDonneVent((int) ((positionX-zonePrevision.getLatitudeHautGauche())/zonePrevision.getPasX()),
+						(int)((positionY-zonePrevision.getLatitudeHautGauche())/zonePrevision.getPasY()));
+				if(vent!=null) {
+					vent.applique(this);
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -36,8 +60,8 @@ public class VisiteurCoefficient implements Visiteur {
 	
 	@Override
 	public void agitSur(Prevision element) {
+		zoneModifier(element);
 		
-		element.getDonneVent(0,0).applique(this);
 	}
 	
 	@Override
