b55c182b83c6c9673547ebfa6926c3d0e189a10c
Faranni <PC-JYN@DESKTOP-NAHFT19>
2018-03-19 23:55:51 +0100

    [mail] envoie mail

--- recuperation_donnees_noaa/src/mail/Mail.java
+++ recuperation_donnees_noaa/src/mail/Mail.java
@@ -123,7 +123,7 @@ public class Mail {
       Properties properties = new Properties();
 
       properties.put("mail.pop3.host", host);
-      properties.put("mail.pop3.port", "995");
+      properties.put("mail.pop3.port", "993");
       properties.put("mail.pop3.starttls.enable", "true");
       Session emailSession = Session.getDefaultInstance(properties);
 
