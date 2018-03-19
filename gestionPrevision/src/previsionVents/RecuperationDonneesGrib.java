7dacd624183fd0d19ed8ae178d531f683b64106c
parlant <parlant@172.28.189.255>
2018-03-19 16:08:41 +0100

    erreurs path

--- gestionPrevision/src/previsionVents/RecuperationDonneesGrib.java
+++ gestionPrevision/src/previsionVents/RecuperationDonneesGrib.java
@@ -25,15 +25,15 @@ public class RecuperationDonneesGrib {
     ZonePrevision zone = listePrevisionTest.getZonePrevision();
     System.out.println("longitude : " + zone.getLongitudeHautGauche());
     System.out.println("latitude : " + zone.getLatitudeHautGauche());
-//    for (int i = 0; i < donnee.length; i++) {
-//      for (int y = 0; y < donnee[0].length; y++) {
-//        
-//        System.out.println("coordonee : (" + i + " " + y + ")(lat: " + zone.getLatitudePosition(i)
-//            + "\tlong: " + zone.getLongitudePosition(y) + " )");
-//        System.out.print("Orientation :" + donnee[i][y].getOrientationVent());
-//        System.out.println("\tVitesse :" + donnee[i][y].getVitesseVent());
-//      }
-//    }
+    for (int i = 0; i < donnee.length; i++) {
+      for (int y = 0; y < donnee[0].length; y++) {
+        
+        System.out.println("coordonee : (" + i + " " + y + ")(lat: " + zone.getLatitudePosition(i)
+            + "\tlong: " + zone.getLongitudePosition(y) + " )");
+        System.out.print("Orientation :" + donnee[i][y].getOrientationVent());
+        System.out.println("\tVitesse :" + donnee[i][y].getVitesseVent());
+      }
+    }
   }
 
   /**
