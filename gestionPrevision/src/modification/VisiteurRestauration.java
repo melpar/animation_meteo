abd5562ff8003db59338b82d67fd950026c7b7aa
scawes <thomas.marescaux@hotmail.fr>
2018-03-19 10:16:26 +0100

    [modification] ajout class

---
+++ gestionPrevision/src/modification/VisiteurRestauration.java
@@ -0,0 +1,45 @@
+package modification;
+
+import previsionVents.DonneeVent;
+import previsionVents.ListePrevision;
+import previsionVents.Prevision;
+import previsionVents.ZonePrevision;
+import visiteur.ElementVisitable;
+import visiteur.Visiteur;
+
+public class VisiteurRestauration implements Visiteur {
+
+  private ListePrevision sauvegarde;
+  
+  @Override
+  public void agitSur(ElementVisitable element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void agitSur(DonneeVent element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void agitSur(ListePrevision element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void agitSur(Prevision element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void agitSur(ZonePrevision element) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  
+}
