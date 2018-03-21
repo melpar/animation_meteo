06184e992d0ed22425bc69863a9d1878a7db5733
parlant <parlant@172.28.189.255>
2018-03-21 16:09:15 +0100

    gestion densite points (4)

--- gestionPrevision/src/carteFX/densite/Zoom.java
+++ gestionPrevision/src/carteFX/densite/Zoom.java
@@ -6,20 +6,25 @@ import carteFX.MapCanvas;
 
 public class Zoom {
   private MapCanvas canvas;
+  private static double deltaZoom;
 
   public Zoom(MapCanvas can) {
     this.canvas = can;
   }
 
   public void zoom(double delta) {
-    ReferencedEnvelope envelope = canvas.getMap().getViewport().getBounds();
-    double percent = delta / canvas.canvas.getWidth();
-    double width = envelope.getWidth();
-    double height = envelope.getHeight();
-    double deltaW = width * percent;
-    double deltaH = height * percent;
-    envelope.expandBy(deltaW, deltaH);
-    AffichageFleches.miseAJour(envelope);
-    canvas.doSetDisplayArea(envelope);
+    if ((!(delta >= 0 && delta > 0)) || (!(delta <= -2000 && delta < 0))) {
+      ReferencedEnvelope envelope = canvas.getMap().getViewport().getBounds();
+      double percent = delta / canvas.canvas.getWidth();
+      double width = envelope.getWidth();
+      double height = envelope.getHeight();
+      double deltaW = width * percent;
+      double deltaH = height * percent;
+      envelope.expandBy(deltaW, deltaH);
+      AffichageFleches.miseAJour(envelope);
+      canvas.doSetDisplayArea(envelope);
+      deltaZoom += delta;
+      System.out.println(deltaZoom);
+    }
   }
 }
